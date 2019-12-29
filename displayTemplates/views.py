9bdeb61c0f979aaf4cd31c074311843d4e24c964
AbdulAhadSiddiqui11 <abdulahadsiddiqui011@gmail.com>
2019-12-29 23:16:52 +0530

    added : login required for templates

--- displayTemplates/views.py
+++ displayTemplates/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, redirect
 from .models import Template
+from django.contrib.auth.models import auth, User
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def index(request):
     templates = Template.objects.all()
     return render(request, 'templates.html', {'templates': templates})
 
-
+@login_required
 def preview(request):
     if request.method == 'POST' :
         val = request.POST['temp_name']
