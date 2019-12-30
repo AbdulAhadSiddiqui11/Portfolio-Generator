cffa2c22a501d7c3c62bc4f099cfdbc401003c9c
AbdulAhadSiddiqui11 <abdulahadsiddiqui011@gmail.com>
2019-12-30 17:22:46 +0530

    Tabular Inline models created for get details app

--- getdetails/admin.py
+++ getdetails/admin.py
@@ -1,12 +1,48 @@
 from django.contrib import admin
-from .models import *
+from .models import UserInfo,Experience,School,Certificate,Volunteering,Skill,Project,Award,Organisation
 
 
 class ExperienceInline(admin.TabularInline):
     model = Experience
     extra = 0
 
+class SchoolInline(admin.TabularInline):
+    model = School
+    extra = 0
+
+class CertificateInline(admin.TabularInline):
+    model = Certificate
+    extra = 0
+
+class VolunteeringInline(admin.TabularInline):
+    model = Volunteering
+    extra = 0
+
+class SkillInline(admin.TabularInline):
+    model = Skill
+    extra = 0
+
+class ProjectInline(admin.TabularInline):
+    model = Project
+    extra = 0
+
+class AwardInline(admin.TabularInline):
+    model = Award
+    extra = 0
+
+class OrganisationInline(admin.TabularInline):
+    model = Organisation
+    extra = 0
+
 class UserInfoAdmin(admin.ModelAdmin):
-    inlines = [ExperienceInline]
+    inlines = [     ExperienceInline,
+                    SchoolInline,
+                    CertificateInline,
+                    VolunteeringInline,
+                    SkillInline,
+                    ProjectInline,
+                    AwardInline,
+                    OrganisationInline,
+                ]
 
 admin.site.register(UserInfo, UserInfoAdmin)
