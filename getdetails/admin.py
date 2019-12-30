50135945ca7354d76abd21cec81a10c237c0ffa6
AbdulAhadSiddiqui11 <abdulahadsiddiqui011@gmail.com>
2019-12-30 16:51:43 +0530

    User Details Models Created!

--- getdetails/admin.py
+++ getdetails/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class ExperienceInline(admin.TabularInline):
+    model = Experience
+    extra = 0
+
+class UserInfoAdmin(admin.ModelAdmin):
+    inlines = [ExperienceInline]
+
+admin.site.register(UserInfo, UserInfoAdmin)
--- getdetails/models.py
+++ getdetails/models.py
@@ -1,3 +1,80 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# TODO: Check the date function -> Add present date function
+
+class UserInfo(models.Model):
+    fname = models.CharField(max_length = 50)
+    lname = models.CharField(max_length = 50)
+    headline = models.CharField(max_length = 250)
+    about = models.TextField()
+    github = models.CharField(max_length=150)
+    linkedin = models.CharField(max_length=150)
+    email = models.CharField(max_length=150)
+    website = models.CharField(max_length=150)
+    photo = models.ImageField(upload_to='user_profiles')
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+
+
+class Experience(models.Model):
+    company = models.CharField(max_length=100)
+    position = models.CharField(max_length=100)
+    startdate = models.DateField()
+    endate = models.DateField()
+    desc = models.TextField(blank=True)
+    userinfo = models.ForeignKey(UserInfo, on_delete = models.CASCADE)
+
+class School(models.Model):
+    school = models.CharField(max_length=100)
+    startdate = models.DateField()
+    endate = models.DateField()
+    desc = models.TextField(blank=True)
+    userinfo = models.ForeignKey(UserInfo, on_delete = models.CASCADE)
+
+class Certificate(models.Model):
+    name = models.CharField(max_length=100)
+    org = models.CharField(max_length=100)
+    issuedate = models.DateField()
+    expiration = models.DateField()
+    cred_id = models.CharField(max_length=100)
+    cred_url = models.CharField(max_length=150)
+    userinfo = models.ForeignKey(UserInfo, on_delete = models.CASCADE)
+
+class Volunteering (models.Model):
+    org = models.CharField(max_length=100)
+    role = models.CharField(max_length=100)
+    startdate = models.DateField()
+    endate = models.DateField()
+    desc = models.TextField(blank=True)
+    userinfo = models.ForeignKey(UserInfo, on_delete = models.CASCADE)
+
+class Skill(models.Model):
+    name = models.CharField(max_length=40)
+    userinfo = models.ForeignKey(UserInfo, on_delete = models.CASCADE)
+
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    desc = models.TextField(blank=True)
+    startdate = models.DateField()
+    endate = models.DateField()
+    link = models.CharField(max_length=150)
+    userinfo = models.ForeignKey(UserInfo, on_delete = models.CASCADE)
+
+class Award(models.Model):
+    name = models.CharField(max_length=100)
+    org = models.CharField(max_length=100)
+    date = models.DateField()
+    desc = models.TextField(blank=True)
+    userinfo = models.ForeignKey(UserInfo, on_delete = models.CASCADE)
+
+class Organisation(models.Model):
+    name = models.CharField(max_length=100)
+    role = models.CharField(max_length=100)
+    startdate = models.DateField()
+    endate = models.DateField()
+    desc = models.TextField(blank=True)
+    userinfo = models.ForeignKey(UserInfo, on_delete = models.CASCADE)
+
+
+
